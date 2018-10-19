5b5dab64db879c9f76474ec884f0cf43ab9e4f3f
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-10-19 13:00:52 -0500

    first parcel app commit

---
+++ local/src/app.js
@@ -0,0 +1,161 @@
+'use strict';
+
+var appData = '../ForBoot/appData';
+
+var fs = require('fs');
+
+if (fs.existsSync('/boot/appData/config.js')) {
+  console.log('on pi');
+  appData = '/boot/appData';
+}
+
+var obtains = [
+  './src/wallControl.js',
+  'µ/commandClient.js',
+  '../piFig/src/utils.js',
+  `${appData}/config.js`,
+];
+
+obtain(obtains, ({ valves }, { MuseControl }, utils, { config })=> {
+  exports.app = {};
+
+  var control = new MuseControl('172.17.69.76');
+
+  //console.log(utils.getIpAddress());
+
+  var _ = 1;
+
+  var test = [[_, _, 0, 0, _, _, 0, 0, _, _, 0, 0],
+              [_, _, 0, 0, _, _, 0, 0, _, _, 0, 0],
+              [0, 0, _, _, 0, 0, _, _, 0, 0, _, 1],
+              [0, 0, _, _, 0, 0, _, _, 0, 0, _, 1],
+              [_, _, 0, 0, _, _, 0, 0, _, _, 0, 0],
+              [_, _, 0, 0, _, _, 0, 0, _, _, 0, 0],
+              [0, 0, _, _, 0, 0, _, _, 0, 0, _, 1],
+              [0, 0, _, _, 0, 0, _, _, 0, 0, _, 1],
+              [_, _, 0, 0, _, _, 0, 0, _, _, 0, 0],
+              [_, _, 0, 0, _, _, 0, 0, _, _, 0, 0],
+              [0, 0, _, _, 0, 0, _, _, 0, 0, _, 1],
+              [0, 0, _, _, 0, 0, _, _, 0, 0, _, 1],
+
+              /////////////////////////////////////
+              [_, _, _, _, 0, 0, 0, 0, _, _, _, _],
+              [_, _, _, _, 0, 0, 0, 0, _, _, _, _],
+              [_, _, _, _, 0, 0, 0, 0, _, _, _, _],
+              [_, _, _, _, 0, 0, 0, 0, _, _, _, _],
+              [0, 0, 0, 0, _, _, _, _, 0, 0, 0, 0],
+              [0, 0, 0, 0, _, _, _, _, 0, 0, 0, 0],
+              [0, 0, 0, 0, _, _, _, _, 0, 0, 0, 0],
+              [0, 0, 0, 0, _, _, _, _, 0, 0, 0, 0],
+              [_, _, _, _, 0, 0, 0, 0, _, _, _, _],
+              [_, _, _, _, 0, 0, 0, 0, _, _, _, _],
+              [_, _, _, _, 0, 0, 0, 0, _, _, _, _],
+              [_, _, _, _, 0, 0, 0, 0, _, _, _, _],
+
+              /////////////////////////////////////
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, _, _, 0, _, _, 0, 0, 0, 0, 0, 0],
+              [_, _, _, _, _, _, _, 0, 0, 0, 0, 0],
+              [_, _, _, _, _, _, _, 0, 0, 0, 0, 0],
+              [0, _, _, _, _, _, 0, 0, 0, 0, 0, 0],
+              [0, 0, _, _, _, 0, 0, _, 0, _, 0, 0],
+              [0, 0, 0, _, 0, 0, _, _, _, _, _, 0],
+              [0, 0, 0, 0, 0, 0, _, _, _, _, _, 0],
+              [0, 0, 0, 0, 0, 0, 0, _, _, _, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, _, 0, 0, 0],
+
+              /////////////////////////////////////
+              [_, _, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, _, _, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, _, _, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, _, _, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, _, _, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, _, _, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, _, _, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, _, _, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, _, _, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, _, _, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, _, _],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, _, _, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, _, _, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, _, _, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, _, _, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, _, _, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, _, _, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, _, _, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, _, _, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, _, _, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [_, _, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+              /////////////////////////////////////
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, _, _, 0, 0, 0, 0, _, _, 0, 0],
+              [0, 0, _, _, 0, 0, 0, 0, _, _, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, _, 0, 0, 0, 0, 0, 0, 0, 0, _, 0],
+              [0, _, _, 0, 0, 0, 0, 0, 0, _, _, 0],
+              [0, 0, _, _, _, _, _, _, _, _, 0, 0],
+              [0, 0, 0, _, _, _, _, _, _, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  var count = 0;
+
+  window.onbeforeunload = ()=> {
+    valves.allOff();
+  };
+
+  exports.app.start = ()=> {
+    control.addListener('drawRow', (pack)=> {
+      valves.rasterRow(pack.data, pack.stamp - control.timeOffset);
+    });
+
+    control.addListener('drawRaster', (pack)=> {
+      valves.drawRaster(pack.data, pack.stamp - control.timeOffset);
+    });
+
+    control.addListener('pixelWidth', (wid)=> {
+      valves.pixel.width = wid;
+    });
+
+    control.addListener('pixelHeight', (hgt)=> {
+      valves.pixel.height = hgt;
+      console.log('Pixel height set to ' + valves.pixel.height);
+    });
+
+    control.connect();
+
+    valves.pixel.height = 60;
+
+    /*let defaultDraw = setInterval(()=> {
+      valves.drawRaster(test, Date.now() + 50);
+    }, (test.length + 10) * valves.pixel.height);*/
+
+    control.onConnect = ()=> {
+      console.log('connected to server');
+      //clearInterval(defaultDraw);
+      control.send({ _id: config._id, ip: utils.getIpAddress() });
+    };
+
+    console.log('started');
+
+    document.onkeydown = (e)=> {
+      let key = e.key.charCodeAt(0) - 97;
+      console.log(key);
+      if (key >= 0 && key < 24) {
+        valves.force(key, 1);
+      }
+    };
+
+    document.onkeyup = (e)=> {
+      let key = e.key.charCodeAt(0) - 97;
+      console.log(key);
+      if (key >= 0 && key < 24) {
+        valves.force(key, 0);
+      }
+    };
+
+  };
+
+  provide(exports);
+});
---
+++ local/src/wallControl.js
@@ -0,0 +1,71 @@
+obtain(['fs', 'rpio'], (fs, rpio)=> {
+  //exports.valves = {};
+
+  //var outs = [2, 3, 4, 17, 18, 27, 22, 23, 24, 10, 9, 25, 11, 8, 7, 0, 1, 5, 6, 12, 13, 19, 16, 26];
+  var outs = [2, 9, 6, 3, 25, 12, 4, 11, 13, 17, 8, 19, 18, 7, 16, 27, 0, 26, 22, 1, 20, 10, 5, 21];
+
+  var valves = function () {
+    var _this = this;
+
+    this.timeOffset = 0;
+    this.pixel = { width: 2, height: 20 };
+
+    rpio.init({ mapping: 'gpio' });
+
+    for (var i = 0; i < outs.length; i++) {
+      let num = outs[i];
+      outs[i] = { pin: num, state: rpio.LOW, scheduled: 0 };
+      rpio.open(outs[i].pin, rpio.OUTPUT, rpio.LOW);
+    }
+
+    _this.allOff = ()=> {
+      for (var i = 0; i < outs.length; i++) {
+        rpio.write(outs[i].pin, rpio.LOW);
+      }
+    };
+
+    _this.force = (num, state)=> {
+      let out = outs[num];
+      rpio.write(out.pin, state ? rpio.HIGH : rpio.LOW);
+    };
+
+    _this.set = (num, length, stamp)=> {
+      let out = outs[num];
+      let delay = Math.max(0, stamp - Date.now());
+      if (stamp > out.scheduled) {
+        if (stamp > out.scheduled) out.scheduled = stamp + length - 5;
+        setTimeout(()=> {
+          out.state = rpio.HIGH;
+          rpio.write(out.pin, rpio.HIGH);
+          setTimeout(()=> {
+            out.state = rpio.LOW;
+            rpio.write(out.pin, rpio.LOW);
+          }, length - 5);
+        }, delay);
+      }
+    };
+
+    _this.rasterRow = (data, stamp)=> {
+      if (data.length == outs.length / _this.pixel.width) {
+        for (var i = 0; i < data.length; i++) {
+          if (data[i]) {
+            for (var j = 0; j < _this.pixel.width; j++) {
+              _this.set(_this.pixel.width * i + j, _this.pixel.height, stamp);
+            }
+          }
+        }
+      }
+    };
+
+    _this.drawRaster = (data, stamp)=> {
+      for (var i = data.length - 1; i >= 0; i--) {
+        _this.rasterRow(data[i], stamp + (((data.length - 1) - i) * _this.pixel.height));
+      }
+    };
+
+  };
+
+  exports.valves = new valves();
+
+  provide(exports);
+});
